b5f89e4619217c82eaf7ef8b72bff7f3f425716a
Herick Pereira <42222113+herickkgb@users.noreply.github.com>
2023-11-07 23:03:39 -0300

    Criando a splashArt do app

--- app/src/main/java/com/herick/appdefilmes/MainActivity.kt
+++ app/src/main/java/com/herick/appdefilmes/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.herick.appdefilmes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.PersistableBundle
+import com.herick.appdefilmes.databinding.ActivityMainBinding
+import com.herick.appdefilmes.view.FormLogin
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        jumpingSplashart()
     }
+
+
+    private fun jumpingSplashart() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, FormLogin::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+
 }
